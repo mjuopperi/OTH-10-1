a0232d07dec51e9145b8fbd53939387024e2b49b
Markus Juopperi <markus.juopperi@kolumbus.fi>
2015-11-03 16:38:58 +0200

    Initial commit

---
+++ kertoma.cpp
@@ -0,0 +1,7 @@
+include "functions.h"
+int kertoma(int n){
+	if(n!=1){
+		return (n * kertoma(n-1));
+	}
+	else return 1;
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "functions.h"
+using namespace std;
+int main(){
+	cout << "Luvun 5 kertoma on " << kertoma(5) << endl;
+	return 0;
+}
